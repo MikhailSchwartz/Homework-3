37b0d6870dc0a77e65e5a7ca66ca8e3e412800a1
Mikhail Shvarts <m.schwarts@bk.ru>
2025-08-31 15:51:58 +0300

    Домашка 2 первая функция

--- src/widget.py
+++ src/widget.py
@@ -1,4 +1,4 @@
-from masks import get_mask_card_number, get_mask_account
+from masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(account_details: str) -> str:
@@ -6,16 +6,24 @@ def mask_account_card(account_details: str) -> str:
     возвращает строку с замаскированным номером"""
 
     account_details_list = account_details.split()
-    type = []
-    number = []
+    types = []
+    numbers = []
 
     for element in account_details_list:
         if element.isalpha():
-            type.append(element)
-        if element.isdigit():
-            number.append(element)
+            types.append(element)
+        elif element.isdigit():
+            numbers.append(element)
 
-    if "счет" or "счёт" in type:
-        return f"{type} {get_mask_account(str(number))}"
+    numbers_str = "".join(numbers)
+    types_str = " ".join(types)
 
-    return f"{' '.join(type)} {get_mask_card_number(str(number))}"
+    if "счет" in types_str.lower() or "счёт" in types_str.lower():
+        return f"{types_str} {get_mask_account(numbers_str)}"
+
+    return f"{types_str} {get_mask_card_number(numbers_str)}"
+
+
+if __name__ == "__main__":
+    print(mask_account_card("Visa Platinum 7000792289606361"))
+    print(mask_account_card("Счет 73654108430135874305"))
