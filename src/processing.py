9f47431bb2eeeb7d1887d117420e3661a1e18da1
Mikhail Shvarts <m.schwarts@bk.ru>
2025-09-08 01:05:49 +0300

    second function filter_by_state

--- src/processing.py
+++ src/processing.py
@@ -9,3 +9,12 @@ def filter_by_state(bank_processing_list: list, state: str = "EXECUTED") -> list
             bank_processing_list_filtred.append(processing)
 
     return bank_processing_list_filtred
+
+
+def sort_by_date(bank_processing_list: list, order: bool = True) -> list:
+    """Функция sort_by_date, которая принимает список словарей и необязательный параметр, задающий порядок сортировки
+    (по умолчанию — убывание). Функция должна возвращать новый список, отсортированный по дате (date)."""
+
+    bank_processing_list_sorted = sorted(bank_processing_list, key=lambda operation: operation['date'], reverse=order)
+
+    return bank_processing_list_sorted
