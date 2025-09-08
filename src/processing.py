2db972146047e684be867314149cff76415e0bfd
Mikhail Shvarts <m.schwarts@bk.ru>
2025-09-08 09:09:11 +0300

    working on corrections

--- src/processing.py
+++ src/processing.py
@@ -1,23 +1,23 @@
-def filter_by_state(bank_processing_list: list, state: str = "EXECUTED") -> list:
+def filter_by_state(banking_oprtations: list, state: str = "EXECUTED") -> list:
     """Функция принимает список словарей с банковскими операциями и возвращает новый список словарей,
     содержащий только те словари, у которых ключ state (по умолчанию 'EXECUTED') соответствует указанному значению."""
 
-    bank_processing_list_filtred = []
+    filtered_banking_operanions = []
 
-    for processing in bank_processing_list:
-        if processing.get("state") == state:
-            bank_processing_list_filtred.append(processing)
+    for operation in banking_oprtations:
+        if operation.get("state") == state:
+            filtered_banking_operanions.append(operation)
 
-    return bank_processing_list_filtred
+    return filtered_banking_operanions
 
 
-def sort_by_date(bank_processing_list: list, order: bool = True) -> list:
+def sort_by_date(banking_operations: list, order: bool = True) -> list:
     """Функция sort_by_date, которая принимает список словарей и необязательный параметр, задающий порядок сортировки
     (по умолчанию — убывание). Функция должна возвращать новый список, отсортированный по дате (date)."""
 
-    bank_processing_list_sorted = sorted(bank_processing_list, key=lambda operation: operation["date"], reverse=order)
+    sorted_banking_operations = sorted(banking_operations, key=lambda operation: operation["date"], reverse=order)
 
-    return bank_processing_list_sorted
+    return sorted_banking_operations
 
 
 if __name__ == "__main__":
@@ -27,4 +27,4 @@ if __name__ == "__main__":
         {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
         {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
     ]
-    print(sort_by_date(bank))
+    print(filter_by_state(bank))
