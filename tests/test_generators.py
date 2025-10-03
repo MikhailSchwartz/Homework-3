2eb9b6236a4b425cfc9921de9b5daf23c17f6ac4
Mikhail Shvarts <m.schwarts@bk.ru>
2025-10-04 00:48:56 +0300

    Test function_2

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -2,4 +2,16 @@ import pytest
 
 from src.generators import filter_by_currency, transaction_descriptions, card_number_generator
 
-@pytest.mark.parametrize()
+
+def test_filter_by_currency_normal(normal_transaction_list: list, normal_result_filter_by_currency_usd: list) -> None:
+    """Тест работы функции при вводе полных данных и валюты USD"""
+
+    result = list(filter_by_currency(normal_transaction_list, "USD"))
+    assert result == normal_result_filter_by_currency_usd
+
+
+def test_filter_by_currency_normal(normal_transaction_list: list, normal_result_filter_by_currency_rub: list) -> None:
+    """Тест работы функции при вводе полных данных и валюты RUB"""
+
+    result = list(filter_by_currency(normal_transaction_list, "RUB"))
+    assert result == normal_result_filter_by_currency_rub
