12cdc821254bb784e8b7844ed6be812f3047c79d
Mikhail Shvarts <m.schwarts@bk.ru>
2025-10-09 16:41:08 +0300

    Decorators_log_func

--- src/decorators.py
+++ src/decorators.py
@@ -5,18 +5,42 @@ def log(filename=None):
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
+            try:
+                result = func(*args, **kwargs)
+                message = f"{func.__name__} ok"
+
+                if filename:
+                    with open(filename, "a") as file:
+                        file.write(message + "\n")
+                else:
+                    print(message)
+
+                return result
+            except Exception as exc_info:
+                message = f"{func.__name__} error: {type(exc_info).__name__}. Inputs: {args}, {kwargs}"
+                if filename:
+                    with open(filename, "a") as file:
+                        file.write(message + "\n")
+                else:
+                    print(message)
 
         return wrapper
+
     return decorator
 
 
-
 if __name__ == "__main__":
-    @log(filename="mylog.txt")
-    def my_function(x, y):
-        return x + y
+#     @log(filename="mylog.txt")
+#     def my_function(x, y):
+#         return x - y
+#
+#     my_function(10, 0)
 
 
-    my_function(1, 2)
+    @log()
+    def function(x, y):
+        """Деление X на Y"""
+        return x / y
 
 
+    function(10, 0)
