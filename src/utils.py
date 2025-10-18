3a85bd2ed60020d02da5eb4e9f1040c7dc263406
Mikhail Shvarts <m.schwarts@bk.ru>
2025-10-18 23:08:13 +0300

    Feature/homework 10 5   log masks 3, readme

--- src/utils.py
+++ src/utils.py
@@ -2,7 +2,7 @@ import json
 import logging
 
 logger = logging.getLogger("utils")
-file_handler = logging.FileHandler("../logs/logs.log", mode="w", encoding="utf-8")
+file_handler = logging.FileHandler("../logs/logs_utils.log", mode="w", encoding="utf-8")
 file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
